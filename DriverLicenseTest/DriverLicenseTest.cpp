6d665bca874418abdb6f59c297eb2b60e7ccaded
Simone <simone.k.christen@gmail.com>
2022-06-25 22:04:56 -0700

    description and readme updated

--- DriverLicenseTest/DriverLicenseTest.cpp
+++ DriverLicenseTest/DriverLicenseTest.cpp
@@ -3,7 +3,7 @@
  by Simone Christen
 
  Takes user input for 20 questions as an array and grades them against the correct answers array.
- Grades the test and informs the user of the result in percentage and whether they achieved a passing score or failed.
+ Grades the test and informs the user of the result in raw score, percentage, and whether they ultimately achieved a passing score or failed.
 
  */
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DriverLicenseTest
+
+ DriverLicenseTest
+ by Simone Christen
+
+ Takes user input for 20 questions as an array and grades them against the correct answers array.
+ Grades the test and informs the user of the result in raw score, percentage, and whether they ultimately achieved a passing score or failed.
